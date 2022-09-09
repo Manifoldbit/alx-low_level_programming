c511b43268e93eb97b7e287f91eb2a5dc0341704
Manifoldbit <iloribabajide204@gmail.com>
2022-09-09 07:20:20 -0700

    Adding task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	strand(time(0));
+
+	int n = rand();
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+}
