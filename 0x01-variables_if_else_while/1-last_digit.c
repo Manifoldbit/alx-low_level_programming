6914797707722ba37ce25ee7db9f6b4ac11fc728
Manifoldbit <iloribabajide204@gmail.com>
2022-09-10 04:43:31 -0700

    Adding task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,8 +7,11 @@
  *
  * Return: ALways 0 (Success)
  */
+
 int main(void)
+
 {
+
 	int n:
 	
 	int nl;
@@ -25,5 +28,7 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, nl);
 	else if (nl < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, nl);
+	
 	return (0);
 
+}
