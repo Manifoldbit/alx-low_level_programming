468e42fed23ef6be2dd111a257c1be31fdcbac29
Manifoldbit <iloribabajide204@gmail.com>
2022-09-10 04:48:14 -0700

    git Adding task 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 
 	nl = n % 10;
 
-	if (nl > 5 )
+	if (nl > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, nl);
 	else if (nl == 0)
 		printf("Last digit of %d is %d and is 0\n", n, nl);
