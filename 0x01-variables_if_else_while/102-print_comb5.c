916ab4ef3f4c975e33f02aea446a7a609b6a4497
Manifoldbit <iloribabajide204@gmail.com>
2022-09-10 11:41:22 -0700

    Adding task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-  * main - Entry point
-  *
-  * Return: Always (Success)
-  */
+ * main - Entry point
+ *
+ * Return: Always 0  (Success)
+ */
 int main(void)
 {
 	int c, i, k, j;
