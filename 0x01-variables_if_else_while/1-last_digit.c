9974a607590fcf73d29fb736f5e87f48b892f9da
Manifoldbit <iloribabajide204@gmail.com>
2022-09-10 05:04:27 -0700

    Correction to task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
 /**
  * main - Entry point
  *
- * Return: ALways 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 
 	int n;
