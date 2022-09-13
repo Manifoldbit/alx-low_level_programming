0a20d89b051141aed24fad85faa89a38d0f736f3
Manifoldbit <iloribabajide204@gmail.com>
2022-09-13 16:12:28 -0700

    Adding task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 
 	for (counter = 0; counter <= 9; counter++)
 	{
-		for (alpha = 'a'; alpha <= 'z'; alpha++)	
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
 		}
