2f83332ee23026c9f4b04f6fa8e307631b890292
Manifoldbit <iloribabajide204@gmail.com>
2022-09-13 08:35:53 -0700

    Adding task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,27 +5,15 @@
   * Return: 0 (Success)
   */
 int main(void)
-{
-	int letter;
 
-	letter = '_';
-	putchar(letter);
-	letter = 'p';
-	putchar(letter);
-	letter = 'u';
-	putchar(letter);
-	letter = 't';
-	putchar(letter);
-	letter = 'c';
-	putchar(letter);
-	letter = 'h';
-	putchar(letter);
-	letter = 'a';
-	putchar(letter);
-	letter = 'r';
-	putchar(letter);
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	putchar('\n');
 	return (0);
 }
-
