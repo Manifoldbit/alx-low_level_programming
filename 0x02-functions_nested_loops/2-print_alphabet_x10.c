b7f73b2eeff4a80e7872abf688005db20e4b816c
Manifoldbit <iloribabajide204@gmail.com>
2022-09-13 15:57:07 -0700

    Adding task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - printing small letters 10times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int counter;
+
+	char alpha;
+
+	for (counter = 0; counter <= 10; counter++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
