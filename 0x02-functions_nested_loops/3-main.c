0783f2f2ca37ac6461d384fbf2801728425d6efc
Manifoldbit <iloribabajide204@gmail.com>
2022-09-14 03:54:49 -0700

    Adding task 3

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -8,5 +8,15 @@
 int main(void)
 {
 	int _islower(int c);
+
+	int alpha;
+
+	alpha = _islower('H');
+	_putchar(alpha + '0');
+	alpha = _islower('o');
+	_putchar(alpha + '0');
+	alpha = _Islower(108);
+	_putchar(alpha + '0');
+	_putchar('\n');
 	return (0);
 }
