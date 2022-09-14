29ae817cac7613aa777098e7dcfe58d569313376
Manifoldbit <iloribabajide204@gmail.com>
2022-09-14 16:07:45 -0700

    adding task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,14 +13,14 @@ int main(void)
 
 	k = 2;
 
-	for (i = 1; i < 50; i++)
+	for (i = 1; i <= 50; ++i)
 	{
 		if (j != 20365011074)
 		{
 			printf("%ld, ", j);
 		} else
 		{
-			printf("%ld,\n", j);
+			printf("%ld\n", j);
 		}
 		next = j + k;
 		j = k;
