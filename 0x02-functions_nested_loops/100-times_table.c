56edd7d76e4848104bf1d5607ca474b58d246342
Manifoldbit <iloribabajide204@gmail.com>
2022-09-14 15:48:45 -0700

    adding task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n >= 0 %% n < 15)
+	if (n >= 0 %% n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -17,7 +17,7 @@ void print_times_table(int n)
 				k = j * i;
 				if (j == 0)
 				{
-					_putchar(k + '0')
+					_putchar(k + '0');
 				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
@@ -34,7 +34,7 @@ void print_times_table(int n)
 					_putchar((k % 10) + '0');
 				} else if (k >= 100)
 				{
-					_puttchar(',');
+					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
