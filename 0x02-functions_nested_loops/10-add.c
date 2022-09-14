31db4e5b9fe7141a4758bf1d3f2fa80727abdb8c
Manifoldbit <iloribabajide204@gmail.com>
2022-09-14 14:44:03 -0700

    Adding task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * add - add two given integers num1, num2
+ * add - add two given numbers
  *
- * Return: The sum of the two given integers
+ * Return: The sum of the two given numbers
  */
 int add(int num1, int num2)
 {
