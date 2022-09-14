f5b4861ea430ae7eb76e6b46039c2344be3d0445
Manifoldbit <iloribabajide204@gmail.com>
2022-09-14 16:11:14 -0700

    adding task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - print the first 50 fibonacci numbers, starting with 1 and 2
+ * main - prints the first 50 fibonacci numbers, starting with 1 and 2
  * followed by a new line
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
