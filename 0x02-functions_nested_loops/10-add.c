08503dc027f57ec638685d4e41897e3581051acb
Manifoldbit <iloribabajide204@gmail.com>
2022-09-14 14:25:58 -0700

    Addding task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * add - add two given integers
+ *
+ * Return: The sum of the two given integers
+ */
+int add(int num1, num2)
+{
+	return (num1 + num2);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+int add(int, int);
+
+/**
+ * main - check the code.
+ *
+ * Returm: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return(0);
+}
