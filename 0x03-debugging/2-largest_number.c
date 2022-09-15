b8f3bf2f0e5bf9136e1600322276707b60bb3cdb
Manifoldbit <iloribabajide204@gmail.com>
2022-09-15 13:16:31 -0700

    Adding task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,14 +1,14 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * largest_number - return the largeest of 3 numbers 
+ * largest_number - return the largest of 3 numbers 
  * @a: first integer
  * @b: second integer
  * @c: third integer
  * Return: largest number
  */
 
-int largest_number(int  a, int b, int c)
+int largest_number(int a, int b, int c)
 {
 	int largest;
 
