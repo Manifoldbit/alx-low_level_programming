6fbd4aad25264d2c143e7c0fb29bbe8b7348d812
Manifoldbit <iloribabajide204@gmail.com>
2022-09-16 16:02:28 -0700

    Adding task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - print a square of # character
+ * @size: the size of the square
+ * Return: void
+ */
+void print_square(int size)
+{
+	int length, int width;
+
+	if (size <= 0)
+		_putchar('\n');
+	
+	for (length = 0; length < size; length++)
+	{
+		for (width = 0; width < size; width++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
