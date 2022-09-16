60de14fe3334369d01ead7e799e8627f2255131d
Manifoldbit <iloribabajide204@gmail.com>
2022-09-16 07:14:05 -0700

    Adding task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/** 
+ * _isupper - checks if the character is uppercase
+ * @c: the character to be determined if its upper or not
+ * Return: 1 if uppercase or 0 if not
+ */
+int _supper(int c)
+{
+	int alpha;
+
+	int count;
+
+	count = 0;
+
+	for (alpha = 65; alpha <= 90; alpha++)
+	{
+		if (c == alpha)
+		{
+			count++;
+			break;
+		}
+	}
+	if (count == 0)
+		return (0);
+	else
+		return (1);
+}
