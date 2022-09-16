2cc3f1940b493c43b776d996124296a1e7f85a02
Manifoldbit <iloribabajide204@gmail.com>
2022-09-16 13:00:27 -0700

    Adding task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplu numbers
+ * @a: number 1
+ * @b: number 1
+ * Return: the multiplication of two numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
