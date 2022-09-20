5071b3e194bafd0fee86df2ed8b6ff3c0ef3f85d
Manifoldbit <iloribabajide204@gmail.com>
2022-09-20 04:29:59 -0700

    adding task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * strcpy - copy the string
+ * @dest: destination string
+ * @src: the source string whic is to be copied
+ * Return: a copied character
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
