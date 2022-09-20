a3f4ee215528d19b1e986b6661970cf4c708eb06
Manifoldbit <iloribabajide204@gmail.com>
2022-09-20 03:16:56 -0700

    Adding task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses the given the string
+ * @s: the string to be reversed
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char rev[500];
+
+	int i;
+
+	int len;
+
+	len = strlen(s) - 1;
+
+	strcpy(rev, s);
+
+	for (i = len; i >= 0; i--)
+	{
+		rev[len - i] = s[i];
+	}
+	strcpy(s, rev);
+}
