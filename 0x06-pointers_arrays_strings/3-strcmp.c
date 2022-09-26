77a35936692442f40c1ae40d96b24cc64a35a00f
Manifoldbit <iloribabajide204@gmail.com>
2022-09-26 05:30:27 -0700

    Adding task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strcmo - function that compares two strings.
+ * _strcmp - function that compares two strings.
  * @s1: string
  * @s2: strmp
- * Return: 0'.
+ * Return: 0. 
  */
 
 int _strcmp(char *s1, char *s2)
