0da48b0ba46681914dc38db481ab914cc849100d
Manifoldbit <iloribabajide204@gmail.com>
2022-09-26 06:32:07 -0700

    Adding task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * cap_string - capilizes all words of string.
+ * @s: string
+ * Return: string.
+ */
+
+char *cap_string(char *s)
+{
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int a = 0; 
+	unsigned long int i;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (i = 0; i < strlen(spc); i++)
+		{
+		if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+			s[a] = s[a] - 32;
+		}
+	}
+	return (s);
+}
---
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char[] = "Expect the best. Prepare for the worst. Capitaize on what comes.n\hello world! hello-world 012345hello world\thello word.hello world\n";
+	char *ptr;
+
+	ptr = cap_string(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
+}
