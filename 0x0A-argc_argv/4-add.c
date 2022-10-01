66aeeb4f1510cd90296ad7efd009e15b7c62b258
Manifoldbit <iloribabajide204@gmail.com>
2022-10-01 09:38:23 -0700

    adding task $

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
 
 	sum = 0;
 
-	for (i = 1; i < argc;, i++)
+	for (i = 1; i < argc ; i++)
 	{
 		for (j = 0; argv[i][j] != '\0' ; j++)
 		{
-			for (argv[i][j]  < 47 || argv[i][j] > 57)
+			if (argv[i][j]  < 47 || argv[i][j] > 57)
 			{
-				printf("Error\n"):
+				printf("Error\n");
 				return (1);
 			}
 		}
