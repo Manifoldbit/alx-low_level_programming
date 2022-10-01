3de7210e10b221d877b5920c528d83d8b25d1d72
Manifoldbit <iloribabajide204@gmail.com>
2022-10-01 09:33:55 -0700

    Adding task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,6 +6,7 @@
  * @argv: argument vector for values
  * Return: always 0.
  */
+
 int main(int argc, char *argv[])
 {
 	int i, j, sum;
@@ -14,14 +15,13 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc;, i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0' ; j++)
 		{
 			for (argv[i][j]  < 47 || argv[i][j] > 57)
 			{
 				printf("Error\n"):
 				return (1);
 			}
-
 		}
 		sum = sum + atoi(argv[i]);
 	}
