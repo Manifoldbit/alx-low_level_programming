679da8a071aeffc647d551171aed159f211876ec
Manifoldbit <iloribabajide204@gmail.com>
2022-10-08 08:34:03 -0700

    Adding task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - hecking a malloc
+ * @b: bytes
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *i;
+
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	else
+		return (i);
+}
