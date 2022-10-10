c38036e7e3d0230b2b91f2a7150da8d642b68966
Manifoldbit <iloribabajide204@gmail.com>
2022-10-10 08:50:13 -0700

    Adding task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - a structure of dog
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif /* MAIN_H */
