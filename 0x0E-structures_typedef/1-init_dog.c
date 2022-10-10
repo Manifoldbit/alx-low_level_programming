df629bbbea4ed949068be075bb3395516eae2315
Manifoldbit <iloribabajide204@gmail.com>
2022-10-10 09:06:11 -0700

    Adding task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include "dog.h"
+/**
+ * init_dog - initialization of the dog structure 
+ * @d: a dog structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner otf the dog
+ * Return: void
+ */
+void init_dog(struct dof *d, char *name, float age, char *owner)
+{
+	struct dog = {.name = name, .age = age, .owner = owner};
+}
