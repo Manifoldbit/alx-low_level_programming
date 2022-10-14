aa932a6afc3cc48c3673494c373fa36a2972c61c
Manifoldbit <iloribabajide204@gmail.com>
2022-10-14 09:07:24 -0700

    Adding task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches index for the given number 
+ * @array: the array of numbers 
+ * @size: the size of an array
+ * @cmp: a function to compare the elements in array
+ * Return: the index of the matched number
+ */
+int int_index(int *array, int size, int(*cmp)(int))
+{
+	int count;
+
+	count = 0;
+	if (size <= 0)
+		return (-1);
+	if (array != NULL && cmp != NULL)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			count = cmp(array[i]);
+			if (count != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
